3284cf2ce79187adcb9472ebfd26fa8bd8f42242
D Andrew Reynhout <reynhout@quesera.com>
2009-07-07 15:20:22 -0400

    initial

---
+++ callme.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <ruby.h>
+
+typedef void* fp(unsigned int);
+typedef struct watcher_struct {
+  char* path;
+  fp* callback;
+  } watcher_struct;
+
+static void cb_internal(int x)
+  { printf("(C:%d)",x); fflush(stdout); }
+
+static void do_watch(struct watcher_struct* w)
+  {
+  struct stat statbuf;
+  time_t time0;
+  int tmp_x;
+
+  time0 = time(NULL);
+  printf("(watching)"); fflush(stdout);
+  while(1)
+    {
+    if (stat(w->path,&statbuf) == 0)
+      {
+      tmp_x = statbuf.st_mtime-time0;
+      unlink(w->path);
+      //w->callback(tmp_x);
+      printf("(firing callback 0x%08x)", (unsigned int)w->callback);
+      fflush(stdout);
+      rb_thread_blocking_region((rb_blocking_function_t *)w->callback, tmp_x, RUBY_UBF_IO, 0);
+      }
+    sleep(1);
+    }
+  }
+
+VALUE watch_path(VALUE self, VALUE v_path, fp* cbfp, unsigned char use_pthreads)
+  {
+  char* path = StringValuePtr(v_path);
+
+  pthread_t watcher_thr;
+  struct watcher_struct* w;
+
+  w = (watcher_struct *)malloc(sizeof(watcher_struct));
+  w->path = path;
+  w->callback = !NIL_P(cbfp) ? cbfp : (fp *)&cb_internal;
+  printf("-- Trigger callback with:\n   touch %s/%s\n",getcwd(NULL,0),w->path);
+  printf("-- Will use callback at address 0x%08x\n",(unsigned int)w->callback);
+  fflush(stdout);
+  if ( use_pthreads )
+    pthread_create(&watcher_thr,NULL,(void*(*)(void*))do_watch,(void*)w);
+  else
+    do_watch(w);
+
+  return self;
+  }
+
+void Init_callme() {
+  VALUE myModule = rb_define_module("CallMeModule");
+  VALUE myClass = rb_define_class_under(myModule, "CallMe", rb_cObject); 
+  rb_define_module_function(myClass, "watch_path", (void *)&watch_path, 3); 
+  }
+
+int main(void)
+  {
+  int i;
+  char* path = "somefile";
+
+  printf("-- C callback cb_internal 0x%08x\n", (unsigned int)&cb_internal);
+  fflush(stdout);
+  watch_path(0,(VALUE)path,(fp *)&cb_internal,1);
+  for ( i=0; i<=30; i++ )
+    { printf("."); fflush(stdout); sleep(1); }
+  printf("done\n");
+  return EXIT_SUCCESS;
+  }
---
+++ runcallme.rb
@@ -0,0 +1,34 @@
+#!/opt/local/bin/ruby1.9
+
+require 'callme'
+include CallMeModule
+
+USE_RUBY_THREADS = false #|| true
+USE_C_THREADS = false || true
+USE_RUBY_CALLBACK = false #|| true
+
+STDOUT.sync = true
+Thread.abort_on_exception = true
+
+cb_proc = USE_RUBY_CALLBACK ? Proc.new { |x| print "(Ruby:#{x})" } : nil
+
+puts "-- Passing callback address (#{cb_proc.inspect})"
+puts "-- Use Ruby threads? #{USE_RUBY_THREADS.to_s}"
+puts "-- Use C threads? #{USE_C_THREADS.to_s}"
+
+Thread.new { while true do print '#'; sleep 1; end } ## heartbeat
+
+if USE_RUBY_THREADS
+  Thread.new { CallMe.watch_path("somefile", cb_proc, USE_C_THREADS) }
+else
+  CallMe.watch_path("somefile", cb_proc, USE_C_THREADS)
+end
+
+time_to_stop = Time.now.to_i + 30
+while time_to_stop > Time.now.to_i do
+  print '.'
+  sleep 0.2
+end
+
+puts "done"
+
