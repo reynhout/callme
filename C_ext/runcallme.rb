c6bcdfb85a0f3177681a04161f2c4ede263f52a4
D Andrew Reynhout <reynhout@quesera.com>
2009-07-13 12:05:57 -0400

    use a constant for the proc, overcautiously.  tweak ignore.

--- C_ext/runcallme.rb
+++ C_ext/runcallme.rb
@@ -17,18 +17,18 @@ USE_C_THREADS = ENV['USE_C_THR'] ? ENV['USE_C_THR'].to_b : true
 USE_RUBY_THREADS = ENV['USE_RUBY_THR'] ? ENV['USE_RUBY_THR'].to_b : false
 USE_C_CALLBACK = ENV['USE_C_CB'] ? ENV['USE_C_CB'].to_b : false
 
-callback = USE_C_CALLBACK ? nil : Proc.new { |x| print "(Ruby:#{x})" }
+Callback = USE_C_CALLBACK ? nil : Proc.new { |x| print "(Ruby:#{x})" }
 
-puts "-- Passing callback  #{callback.inspect}"
+puts "-- Passing Callback  #{Callback.inspect}"
 puts "-- Use Ruby threads? #{USE_RUBY_THREADS.to_s}"
 puts "-- Use C threads?    #{USE_C_THREADS.to_s}"
 
 Thread.new { while true do print '#'; sleep 1; end } ## heartbeat
 
 if USE_RUBY_THREADS
-  Thread.new { CallMe.watch_path("somefile", callback, USE_C_THREADS) }
+  Thread.new { CallMe.watch_path("somefile", Callback, USE_C_THREADS) }
 else
-  CallMe.watch_path("somefile", callback, USE_C_THREADS)
+  CallMe.watch_path("somefile", Callback, USE_C_THREADS)
 end
 
 time_to_stop = Time.now.to_i + 30
