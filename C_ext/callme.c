df168dfd3b1e316992f3e56f3a43e61583c802d8
D Andrew Reynhout <reynhout@quesera.com>
2009-07-18 21:02:10 -0400

    propagate newly-working status back into stat version

--- C_ext/callme.c
+++ C_ext/callme.c
@@ -6,77 +6,62 @@
 #include <sys/stat.h>
 #include <ruby.h>
 
-//typedef void* fp(unsigned int);
-typedef void (*fp)(int);
+typedef struct watcher_data {
+  char *path;
+  void *cb;
+  int s;
+  } watcher_data;
 
-typedef struct watcher_struct {
-  char* path;
-  fp callback;
-  } watcher_struct;
+static void cb_native(struct watcher_data *w)
+  { printf("(C:%d)", w->s); fflush(stdout); }
 
-static void cb_internal(int x)
-  { printf("(C:%d)",x); fflush(stdout); }
+static void cb_ruby(struct watcher_data *w)
+  { rb_funcall((int)w->cb, rb_intern("call"), 1, INT2FIX(w->s)); }
 
-static void do_watch(struct watcher_struct* w)
+static void do_watch(struct watcher_data *w)
   {
   struct stat statbuf;
-  time_t time0;
-  int tmp_x;
+  time_t time0 = time(NULL);
 
-  time0 = time(NULL);
-  printf("(watching)"); fflush(stdout);
   while(1)
     {
-    if (stat(w->path,&statbuf) == 0)
+    if (stat(w->path, &statbuf) == 0)
       {
-      tmp_x = statbuf.st_mtime-time0;
+      w->s = statbuf.st_mtime-time0;
       unlink(w->path);
-      printf("(firing callback 0x%08x)", (unsigned int)w->callback);
-      fflush(stdout);
-      printf("(tmp_x is %d)",tmp_x); fflush(stdout);
-      //w->callback(tmp_x);
-      rb_thread_blocking_region((rb_blocking_function_t *)w->callback, &tmp_x, RUBY_UBF_IO, 0);
+      rb_thread_create((void *)cb_ruby, w);
       }
     sleep(1);
     }
   }
 
-VALUE watch_path(VALUE self, VALUE v_path, fp cb_fp, unsigned char use_pthreads)
+static void watch_path(VALUE self, VALUE v_path, void *cb_fp)
   {
-  char* path = StringValuePtr(v_path);
+  char *path = StringValuePtr(v_path);
 
   pthread_t watcher_thr;
-  struct watcher_struct* w;
+  struct watcher_data *w;
 
-  w = (watcher_struct *)malloc(sizeof(watcher_struct));
+  w = (watcher_data *)malloc(sizeof(watcher_data));
   w->path = strdup(path);
-  w->callback = !NIL_P(cb_fp) ? cb_fp : (fp)cb_internal;
-  printf("-- Trigger callback with:\n   touch %s/%s\n",getcwd(NULL,0),w->path);
-  printf("-- Will use callback at address 0x%08x\n",(unsigned int)w->callback);
-  printf("-- use_pthreads is %d\n", use_pthreads);
-  fflush(stdout);
-  if ( use_pthreads )
-    pthread_create(&watcher_thr,NULL,(void*(*)(void*))do_watch,(void*)w);
-  else
-    do_watch(w);
-
-  return self;
+  w->cb = cb_fp;
+  printf("-- Using callback at 0x%08x\n", (unsigned int)w->cb);
+  printf("-- Trigger with:\n   touch %s/%s\n", getcwd(NULL, 0), w->path);
+  pthread_create(&watcher_thr, NULL, (void *(*)(void *))do_watch, w);
   }
 
 void Init_callme() {
   VALUE myModule = rb_define_module("CallMeModule");
   VALUE myClass = rb_define_class_under(myModule, "CallMe", rb_cObject); 
-  rb_define_module_function(myClass, "watch_path", (void *)&watch_path, 3); 
+  rb_define_module_function(myClass, "watch_path", (void *)&watch_path, 2); 
   }
 
 int main(void)
   {
   int i;
-  char* path = "somefile";
+  char *path = "somefile";
 
-  printf("-- C callback cb_internal 0x%08x\n", (unsigned int)&cb_internal);
-  fflush(stdout);
-  watch_path(0,(VALUE)path,(fp)cb_internal,1);
+  watch_path(0, (VALUE)path, (void *)cb_native);
   for ( i=0; i<=30; i++ )
     { printf("."); fflush(stdout); sleep(1); }
   printf("done\n");
--- C_ext/runcallme.rb
+++ C_ext/runcallme.rb
@@ -1,41 +1,19 @@
 #!/opt/local/bin/ruby1.9
 
-#require 'debug'
 require 'callme'
 include CallMeModule
-
-class Object
-  def to_b
-    self.to_s.match(/^(|false|no|0)$/i) ? false : true
-  end
-end
 
 STDOUT.sync = true
 Thread.abort_on_exception = true
 
-USE_C_THREADS = ENV['USE_C_THR'] ? ENV['USE_C_THR'].to_b : true
-USE_RUBY_THREADS = ENV['USE_RUBY_THR'] ? ENV['USE_RUBY_THR'].to_b : false
-USE_C_CALLBACK = ENV['USE_C_CB'] ? ENV['USE_C_CB'].to_b : false
+Callback = Proc.new { |x| print "(Ruby:#{x})" }
+puts "-- Passing Callback #{Callback.inspect}"
 
-Callback = USE_C_CALLBACK ? nil : Proc.new { |x| print "(Ruby:#{x})" }
+time0 = Time.now.to_i
+Thread.new { while true do print "[#{Time.now.to_i - time0}]"; sleep 1; end }
 
-puts "-- Passing Callback  #{Callback.inspect}"
-puts "-- Use Ruby threads? #{USE_RUBY_THREADS.to_s}"
-puts "-- Use C threads?    #{USE_C_THREADS.to_s}"
+CallMe.watch_path("somefile", Callback)
 
-Thread.new { while true do print '#'; sleep 1; end } ## heartbeat
-
-if USE_RUBY_THREADS
-  Thread.new { CallMe.watch_path("somefile", Callback, USE_C_THREADS) }
-else
-  CallMe.watch_path("somefile", Callback, USE_C_THREADS)
-end
-
-time_to_stop = Time.now.to_i + 30
-while time_to_stop > Time.now.to_i do
-  print '.'
-  sleep 0.2
-end
-
+while Time.now.to_i < time0 + 30 do print '.'; sleep 0.5 end
 puts "done"
 
